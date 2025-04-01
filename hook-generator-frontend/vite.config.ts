51feefb696bff933b84ae1b9c553c3c3aba99385
Jonathan Bannholtzer <bannholtzer.j@gmail.com>
2025-04-01 16:32:46 +0400

    Config: use VITE_API_BASE_URL in production, fallback to localhost in dev for fetchHook 2

--- hook-generator-frontend/vite.config.ts
+++ hook-generator-frontend/vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// ✅ Pas besoin de __dirname ici
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // ✅ important
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
